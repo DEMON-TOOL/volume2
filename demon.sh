dc7c354eb80535ec7d1a2be3c5d4ef7dd8efe304
Demon-Tool <75564402+DEMON-TOOL@users.noreply.github.com>
2020-12-18 11:54:37 +0630

    Add files via upload

---
+++ demon.sh
@@ -0,0 +1,182 @@
+user=demon
+password=demon
+echo -e "\e[1;35m"
+read -p " Enter User Name: " name
+sleep 0.2
+read -p " Enter Your Password: " pass 
+if [[ $name == $user && $pass == $password  ]];then 
+echo""
+echo -e " \e[1;32m Please Wait. \e[0m "
+sleep 1
+for x in {1..100};do
+echo ""
+sleep 0.2
+echo -e " \e[3;2m $ ❤️ loading...$x% \e[0m "
+done
+echo -e " \e[1;94m
+(1).Basic Packages \e[0m "
+sleep 0.5
+echo -e " \e[1;94m
+(2).Download Other Tools \e[0m "
+sleep 0.5
+echo -e " \e[1;94m
+(3).Follow Me In Facebook! \e[0m "
+sleep 0.5
+echo -e " \e[1;94m
+(4).Exit \e[0m "
+sleep 1
+echo -e "\e[1;31m"
+read -p " What do you want? " input
+echo""
+case $input in
+1)
+sleep 1
+pkg update && pkg upgrade
+pkg install python
+pkg installpython2
+pkg install python3
+pkg install git
+pkg install wget
+pkg install sudo
+pkg install bash
+pkg install ruby
+ pkg install openssh
+ pkg install wget
+ pkg install pip
+ pkg install pip2
+ pip2 install requests
+pip2 install mechanize
+ pkg install php
+ pip2 install lolcat
+apt install sl
+apt install libcaca
+apt install fortune
+apt install toilet
+apt install w3m
+apt install cmatrix
+apt install cowsay
+apt-get update -y
+ apt-get upgrade -y
+ pkg install python -y 
+ pkg install python2 -y
+pkg install git -y
+pkg update && pkg upgrade -y
+pkg install python
+ pkg install pip
+pip install --upgrade pip
+pip install random_useragent
+ pkg install php
+pkg install git
+ pkg install nano
+pip install rsa
+ pip install bs4
+ pip install cbpro
+ pip install requests
+pip install colorama
+ pip install telethon
+ pip install tabulate
+ pip install pyfiglet
+pip install lolcat
+pkg install scapy
+pip install scapy 
+pip2 install scapy
+pkg install mariadb
+pkg install nano
+pip2 install -r kntd.txt 
+cmatrix ;;
+2)
+sleep 1
+https://github.com/LUC1F3R818/Advance-tool
+https://github.com/NyeinPyae969/toolcollection
+git clone https://github.com/BotolMehedi/bint
+git clone https://github.com/LUC1F3R818/Advance-tool
+git clone https://github.com/lovehacker404/BlackMafia
+git clone https://github.com/rajkumardusad/Too-X.git
+git clone https://github.com/Xeit666h05t/CrewBot
+git clone https://github.com/UndeadSec/checkURL
+git clone https://github.com/Ranginang67/DarkFly-Tool 
+git clone https://github.com/noob-hackers/tunnel
+git clone https://github.com/amerlaceset/Virus4
+git clone https://github.com/termuxprofessor/insfollow
+git clone https://github.com/BlackTiger-Error404/Hero.git
+git clone ttpss://github.com/skyblueee/sqli-labs-php7
+git clone https://github.com/ridwanirawan/lowbait
+git clone https://github.com/ztlord/facebook
+git clone https://github.com/binyamin-binni/bxi
+git clone https://github.com/htr-tech/zphisher
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3
+git clone https://github.com/sqlmapproject/sqlmap
+git clone https://github.com/oo-ozo/fb-hack
+git clonehttps://github.com/NyeinPyae969/toolcollection
+git clone https://github.com/LimitQueenProject/Empas-Limit
+git clone https://github.com/novalattasya/Tool-N
+git clone https://github.com/ciku370/OSIF
+git clone https://github.com/esc0rtd3w/wifi-hacker
+git clone https://github.com/perijoko14/Dark-fb
+git clone https://github.com/vpphacker/ipinfo.git
+git clone https://github.com/Bhai4You/Termux-Banner
+git clone https://github.com/Mr-G0B3Y/GbyFB.git
+git clone https://github.com/remo7777/T-Header
+git clone https://github.com/An0nUD4Y/shellphish
+git clone https://github.com/Cesar-Hack-Gray/SocialSploit
+git clone https://github.com/N74NK/MLBBTOOL
+git clone https://github.com/CyberMrlink/Ddos404
+git clone https://github.com/Tech-abm/Abm-Tool
+git clone https://github.com/XIDIXP/World
+git clone ttpss://github.com/Bhai4you/Termux-Ba...
+git clone https://github.com/DynamicMirror/fbbrute
+git clone https://github.com/rajkumardusad/onex.git
+git clone https://github.com/TheSploit/Hack-Cam_Phone
+git clone https://github.com/evait-security/weeman.git
+git clone https://github.com/Ha3MrX/DDos-Attack.git
+git clone ttpss://github.com/Al2VyN/Brute-Gmail.git
+git clone https://github.com/TheSpeedX/TBomb.git
+git clone https://github.com/khansaad1275/Termux-Games.git
+git clone https://github.com/dAYOShACKER505/AXOM-FB-HACK
+git clone https://github.com/Somi190/reallover.git
+git clone https://github.com/vpphacker/ipinfo.git
+git clone https://github.com/kinghacker0/WishFish
+git clone https://gitHub.com/XCHADXFAQ77X/XERXES.git
+git clonehttps://githubcom/IAmBlackHacker/Facebook-BruteForce
+git clone https://github.com/vpphacker/ipinfo.git
+git clone https://github.com/perijoko14/Dark-fb
+git clone https://github.com/esc0rtd3w/wifi-hacker
+git clone https://github.com/novalattasya/Tool-N
+git clone https://github.com/joker25000/Devploit-
+git clone https://github.com/darkwarrior3/tstyling
+git clone https://github.com/ihebski/angryFuzzer
+git clone https://github.com/htr-tech/haxorbd
+git clone https://gitlab.com/botolbaba/bind
+git clone https://gitlab.com/botolbaba/b11
+git clone https://gitlab.com/botolbaba/battack
+git clone https://github.com/pashayogi/SETAN
+git clone https://github.com/RazorKenway/otp.git
+git clone https://gitHub.com/XCHADXFAQ77X/XERXES.git
+git clone https://github.com/kweethite/calculator.git
+git clone https://github.com/Jail3/Facebook-Reporer.git 
+git clone https://github.com/Bhai4You/Termux-Banner
+git clone https://github.com/lovehacker404/World
+git clone 
+git clone https://github.com/hangyi00789/Admin-Panel-Finder
+git clone https://github.com/dAYOShACKER505/AXOM-FB-HACK
+cacafire
+;;
+3)
+sleep 1
+xdg-open https://www.facebook.com/welcome00o ;;
+4)
+sleep 1
+clear
+sl
+;;
+*)
+sleep 1
+echo -e " \e[1;32m Sorry Your Choice is not Include. \e[0m ";;
+esac
+echo ""
+else
+echo ""
+sleep 1
+echo -e " \e[1;31m Incorrect Password Or Username \e[0m "
+fi 
+#&& and
